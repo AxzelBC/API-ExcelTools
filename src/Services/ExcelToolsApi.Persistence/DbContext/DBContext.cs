a985db49c4297c087531c05bd67ace3a5c879a45
AxzelBC <alejo13580@gmail.com>
2024-04-07 18:34:34 -0500

    Conexión de DB a .env

--- src/Services/ExcelToolsApi.Persistence/DbContext/DBContext.cs
+++ src/Services/ExcelToolsApi.Persistence/DbContext/DBContext.cs
@@ -8,9 +8,11 @@ public class ApiDbContext : DbContext
     public DbSet<TaskModel> Task { get; set; }
 
     //public string DbPath { get; }
+    private readonly string Connection;
 
     public ApiDbContext()
     {
+        Connection = Environment.GetEnvironmentVariable("CONNECTION_DB") ?? "";
         //var folder = Environment.SpecialFolder.LocalApplicationData;
         //var path = Environment.GetFolderPath(folder);
         //DbPath = Path.Join(path, "tasks.db");
@@ -19,5 +21,5 @@ public class ApiDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder options)
         //=> options.UseSqlite($"Data Source={DbPath}");
-        => options.UseNpgsql(@"Host=exceltools.database:5432;Username=postgres;Password=postgres;Database=tareasdb");
+        => options.UseNpgsql(@Connection);
 }
