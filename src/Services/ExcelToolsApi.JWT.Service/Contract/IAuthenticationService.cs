f58a0dac26221c8a411f9343a7a2d1b8cfae619b
jonnier <jonnierosjitas@gmail.com>
2024-04-09 17:12:26 -0500

    Add AuthenticationService Interface and create Register Adapter of Authentication response and IResquest type

---
+++ src/Services/ExcelToolsApi.JWT.Service/Contract/IAuthenticationService.cs
@@ -0,0 +1,12 @@
+﻿using ExcelToolsApi.Domain.DTO;
+using ExcelToolsApi.Domain.Request;
+using ExcelToolsApi.Domain.Response;
+using ExcelToolsApi.JWT.Service.Commands.CreateUser;
+
+namespace ExcelToolsApi.JWT.Service.Contract;
+
+public interface IAuthenticationService
+{
+    Task<AuthenticationResponse> Register(AuthenticationRegisterAdapter request);
+    //AuthenticationResponse Login(LoginRequestDTO loginRequestDTO);
+}
---
+++ src/Services/ExcelToolsApi.JWT.Service/Extension/AuthenticationRegisterAdapter.cs
@@ -0,0 +1,12 @@
+﻿using ExcelToolsApi.Domain.Response;
+using MediatR;
+
+namespace ExcelToolsApi.JWT.Service;
+public class AuthenticationRegisterAdapter : IRequest<AuthenticationResponse>
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+
+}
