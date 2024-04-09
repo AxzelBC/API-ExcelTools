851ae8587b0f734ad5c0d44f6487c8734df8a771
jonnier <jonnierosjitas@gmail.com>
2024-04-09 17:16:22 -0500

    Fix: Authentication Service implementation

--- src/Services/ExcelToolsApi.JWT.Service/Implementation/AuthenticationService.cs
+++ src/Services/ExcelToolsApi.JWT.Service/Implementation/AuthenticationService.cs
@@ -1,30 +1,40 @@
-﻿namespace ExcelToolsApi.JWT.Service.Implementation;
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using ExcelToolsApi.Domain.DTO;
+using ExcelToolsApi.Domain.Response;
+using ExcelToolsApi.JWT.Service.Contract;
+using MediatR;
 
-public class AuthenticationService : IAuthenticationService
+namespace ExcelToolsApi.JWT.Service.Implementation
 {
-    public AuthenticationResult Register(string firstName, string lastName, string email, string password)
+    public class AuthenticationService : IRequestHandler<AuthenticationRegisterAdapter, AuthenticationResponse>, IAuthenticationService
     {
-        return new AuthenticationResult(
-             Guid.NewGuid(),
-             firstName,
-             lastName,
-             email,
-             password,
-             "token"
-             );
+
+
+        public Task<AuthenticationResponse> Handle(AuthenticationRegisterAdapter request, CancellationToken cancellationToken)
+        {
+            // Lógica de registro aquí...
+            // Aquí se supone que se realiza el registro del usuario en la base de datos o
+            // en algún otro sistema de autenticación.
+
+            var response = new AuthenticationResponse
+            {
+                Id = Guid.NewGuid(), // Genera un nuevo GUID aleatorio para el ID
+                FirstName = request.FirstName,
+                LastName = request.LastName,
+                Email = request.Email,
+                Token = "token123" // Genera un nuevo GUID aleatorio como token
+            };
+
+            // Devuelve el objeto AuthenticationResponse encapsulado en una tarea completada
+            return Task.FromResult(response);
+        }
+
+        public Task<AuthenticationResponse> Register(AuthenticationRegisterAdapter request)
+        {
+            // Aquí delegamos el manejo de la solicitud al método Handle definido anteriormente
+            return Handle(request, new CancellationToken());
+        }
     }
-
-    public AuthenticationResult Login(string email, string password)
-    {
-        return new AuthenticationResult(
-            Guid.NewGuid(),
-            "firstName",
-            "lastName",
-            email,
-            password,
-            "token"
-        );
-    }
-
-
 }
