44243f02daef1730f01c8dea7279b1d45b981391
jonnier <jonnierosjitas@gmail.com>
2024-04-08 19:49:23 -0500

    Add Authentication Controller: This controller contains actions Register and Login that use request data to invoke methods in IAuthenticationService for user authentication.

---
+++ src/Services/ExcelTools.API/Controllers/JWT/AutheticationController.cs
@@ -0,0 +1,54 @@
+﻿using ExcelToolsApi.JWT.Service.Contract;
+using ExcelToolsApi.JWT.Service.Implementation;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExcelTools.API.Controller;
+
+[Route("api/auth")]
+[ApiController]
+public class AutheticationController : ControllerBase
+{
+    private readonly IAuthenticationService _authenticationService;
+
+    public AutheticationController(IAuthenticationService auth)
+    {
+        _authenticationService = auth;
+    }
+
+    [HttpPost("register")]
+    public IActionResult Register(RegisterResquest request)
+    {
+        var authResult = _authenticationService.Register(
+            request.FirstName,
+            request.LastName,
+            request.Email,
+            request.Password
+            );
+        var response = new AuthenticationResponse(
+            authResult.Id,
+            authResult.FirstName,
+            authResult.LastName,
+            authResult.Email,
+            authResult.Token
+        );
+        return Ok(response);
+    }
+
+    [HttpPost("login")]
+    public IActionResult Login(LoginResquest request)
+    {
+        var authResult = _authenticationService.Login(
+            request.Email,
+            request.Password
+            );
+        var response = new AuthenticationResponse(
+            authResult.Id,
+            authResult.FirstName,
+            authResult.LastName,
+            authResult.Email,
+            authResult.Token
+        );
+        return Ok(response);
+    }
+
+}
