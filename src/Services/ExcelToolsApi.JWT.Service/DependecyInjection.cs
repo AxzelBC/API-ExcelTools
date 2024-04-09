4f5fe8092bf0de4004675300b12a229aa554a486
jonnier <jonnierosjitas@gmail.com>
2024-04-09 17:18:08 -0500

    Fix denpendency injection issue

--- src/Services/ExcelToolsApi.JWT.Service/DependecyInjection.cs
+++ src/Services/ExcelToolsApi.JWT.Service/DependecyInjection.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+﻿using ExcelToolsApi.JWT.Service.Contract;
 using ExcelToolsApi.JWT.Service.Implementation;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
 
 namespace ExcelToolsApi.JWT.Service;
 
@@ -8,7 +10,11 @@ public static class DependecyInjection
     // Inyeccion de dependencia de este servicio, para que el program.cs quede mas limpio
     public static IServiceCollection AddJWTService(this IServiceCollection services)
     {
+        // Registra AuthenticationService como implementación de IAuthenticationService
         services.AddScoped<IAuthenticationService, AuthenticationService>();
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetEntryAssembly()));
+        // Registra CreateUserCommandHandler como manejador de la solicitud AuthenticationRegisterAdapter
+        //services.AddScoped<IRequestHandler<AuthenticationRegisterAdapter, AuthenticationResponse>, CreateUserCommandHandler>();
 
         return services;
     }
