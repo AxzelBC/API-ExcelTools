83083ff84115c2ee7eb5d926663a221e0922dbe6
jonnier <jonnierosjitas@gmail.com>
2024-04-09 17:14:43 -0500

    Implement User Command Handler and create the validator file

---
+++ src/Services/ExcelToolsApi.JWT.Service/Feature/Commands/CreateUser/CreateUserCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using ExcelToolsApi.Domain.Response;
+using ExcelToolsApi.JWT.Service.Contract;
+using MediatR;
+
+namespace ExcelToolsApi.JWT.Service.Commands.CreateUser;
+
+
+// TRequest,TResponse
+public class CreateUserCommandHandler : IRequestHandler<AuthenticationRegisterAdapter, AuthenticationResponse>
+{
+    #region private fields
+    private readonly IAuthenticationService _authenticationService;
+
+    #endregion private fields
+    public CreateUserCommandHandler(IAuthenticationService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+
+    public async Task<AuthenticationResponse> Handle(AuthenticationRegisterAdapter request, CancellationToken cancellationToken)
+    {
+        var response = await _authenticationService.Register(request);
+        return response;
+    }
+}
