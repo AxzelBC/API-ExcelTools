54463fe5b3afb45a1599979c4fa0d2b2f51d25d6
jonnier <jonnierosjitas@gmail.com>
2024-05-02 20:52:48 -0500

    fix authentication propierties interfaces

--- src/Services/ExcelToolsApi.JWT.Service/Contract/IAuthenticationService.cs
+++ src/Services/ExcelToolsApi.JWT.Service/Contract/IAuthenticationService.cs
@@ -1,12 +1,10 @@
-﻿using ExcelToolsApi.Domain;
-using ExcelToolsApi.Domain.DTO;
-using ExcelToolsApi.Domain.Response;
+﻿using ExcelToolsApi.Domain.Response;
 
 namespace ExcelToolsApi.JWT.Service.Contract;
 
 public interface IAuthenticationService
 {
     Task<AuthenticationResponse> Register(AuthenticationRegisterAdapter request);
-    Task<AuthenticationResponse> Login(LoginRequestDTO loginRequestDTO);
-    Task<AuthenticationResponse> RenovateToken(TokenRequestDTO tokenRequestDTO);
+    Task<AuthenticationResponse> Login(AuthenticationLoginAdapter request);
+    Task<AuthenticationResponse> RenovateToken(AuthenticationTokenRequestAdapter request);
 }
