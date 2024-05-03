43a26e21747106b98fd69462ec7567723b405f19
jonnier <jonnierosjitas@gmail.com>
2024-05-02 21:23:17 -0500

    fix: resolve dependency issue

--- src/Services/ExcelToolsApi.Persistence/DependencyInjection.cs
+++ src/Services/ExcelToolsApi.Persistence/DependencyInjection.cs
@@ -1,7 +1,4 @@
-﻿using ExcelToolsApi.Persistence.DB;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
+﻿using Microsoft.Extensions.DependencyInjection;
 public static class DependencyInjection
 {
     public static IServiceCollection AddPersistence(this IServiceCollection services)
